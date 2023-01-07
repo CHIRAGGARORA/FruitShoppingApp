f80a0b54917ba235c7b99985a4460d73b68465bf
Chirag Arora <arora2002chirag@gmail.com>
2023-01-07 15:50:09 +0530

    Initial Commit

---
+++ FruitShoppingApp/FruitShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitShoppingAppApp.swift
+//  FruitShoppingApp
+//
+//  Created by chirag arora on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
